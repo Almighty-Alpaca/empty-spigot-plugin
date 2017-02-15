02a31ee3dc9200b64d2849de9747faed5ee7d623
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2017-02-15 12:25:43 +0100

    Update

---
+++ src/main/java/org/example/project/MainClass.java
@@ -0,0 +1,12 @@
+package org.example.project;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainClass extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		System.out.println("Plugin enabled!");
+	}
+
+}
